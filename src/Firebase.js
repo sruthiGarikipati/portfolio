84294f9101a46cc0344a8f3317c9b60d2f727a68
Sruthi Garikipati <garikipatisruthi9542@gmail.com>
2024-08-21 16:11:53 +0530

    Update Firebase config to use environment variables

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,15 +5,17 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCaqJ8HojXgAGGgSQYg3Ay3WKubtsDhcdY",
-  authDomain: "login-4dec7.firebaseapp.com",
-  databaseURL: "https://login-4dec7-default-rtdb.firebaseio.com",
-  projectId: "login-4dec7",
-  storageBucket: "login-4dec7.appspot.com",
-  messagingSenderId: "360106804471",
-  appId: "1:360106804471:web:5d462f6127639766ec3ff2",
-  measurementId: "G-51V8RTZE6J"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
@@ -21,7 +23,10 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { db, auth , storage };
+export { db, auth, storage };
+
+
+
 
 
 
